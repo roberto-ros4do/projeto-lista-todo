a8dec686ad6452a1399d6cdfb8bd571cdd904d57
Roberto Rosado <robertorosadoacd@gmail.com>
2026-04-28 19:38:16 -0300

    Primeira Versão

---
+++ listatarefas.py
@@ -0,0 +1,65 @@
+def listar(agenda):
+   print('------LISTA-------')
+   if not agenda:
+            print('Lista vazia')
+            return
+   
+   for i, tarefa in enumerate(agenda):
+        if tarefa["feito"]:
+            status = 'X'
+        else:
+            status = ' '
+        print(f'[{i}] {tarefa["nome"]} [{status}]')
+
+agenda = []
+while True:
+  print('======MENU=======')
+  print('[1] ADICIONAR')
+  print('[2] LISTAR ITENS')
+  print('[3] REMOVER')
+  print('[4] MARCAR COMO CONCLUIDO')
+  print('[0] SAIR')
+  try:
+      funcao = int(input('INSIRA QUAL FUNÇÃO DESEJA REALIZAR: '))
+      match funcao:
+        case 1:
+          afazer = input('Adicione um afazer ')
+          agenda.append({'nome':afazer, 'feito': False})
+          print('Adicionado com sucesso! ')
+        case 2:
+            listar(agenda)
+
+        case 3:
+          listar(agenda)
+          remover = int(input('Qual deseja remover?: '))
+          if 0<= remover< len(agenda):
+                agenda.pop(remover)
+                print('Removido com sucesso! ')
+          else:
+                print(f'Insira um número entre 0 e {len(agenda) - 1}')
+          listar(agenda)
+
+        case 4:
+            listar(agenda)
+            concluido = int(input('Qual deseja marcar como concluido?: '))
+            if 0<= concluido < len(agenda):
+                   agenda[concluido]["feito"] = True
+                   print('Marcado com sucesso! ')
+            else:
+                print(f'Insira um número entre 0 e {len(agenda) - 1}')
+            listar(agenda)
+
+        case 0:
+            print('Saindo...')
+            break
+          
+        case _:
+            print('INVÁLIDO, INSIRA SOMENTE NÚMEROS INTEIROS DE 0 A 4')
+
+  except ValueError:
+      print('INVÁLIDO, INSIRA SOMENTE MÚMEROS INTEIROS DE 0 A 4')
+  continuar = input('DESEJA REALIZAR MAIS FUNÇÕES? ')
+  if continuar.lower() != 's' and continuar.lower() != 'sim':
+     break
+  else:
+      continue
